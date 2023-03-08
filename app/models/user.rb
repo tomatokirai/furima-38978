623983b1b10108f795d76efac212b5c7ed82c580
tomatokirai <124859494+tomatokirai@users.noreply.github.com>
2023-03-08 19:43:36 +0900

    image導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :items
+  has_many :purchase_records
+
+  validates :nickname, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'を入力してください' }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'を入力してください' }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'を入力してください' }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶ一-]+\z/, message: 'を入力してください' }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶ一-]+\z/, message: '' }
+  validates :birth_day, presence: true
 end
