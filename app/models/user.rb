e2f8bba8a0a436dd12e56aadbf1b830f55555d42
tomatokirai <124859494+tomatokirai@users.noreply.github.com>
2023-03-08 21:22:00 +0900

    トップページ作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,10 @@ class User < ApplicationRecord
   has_many :items
   has_many :purchase_records
 
-  validates :nickname, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'を入力してください' }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'を入力してください' }
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'を入力してください' }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶ一-]+\z/, message: 'を入力してください' }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶ一-]+\z/, message: '' }
+  validates :nickname, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
   validates :birth_day, presence: true
 end
