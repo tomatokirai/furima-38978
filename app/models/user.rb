3dd0577067c0b1e723cccb48a3ac948469c52bff
tomatokirai <124859494+tomatokirai@users.noreply.github.com>
2023-03-09 10:58:24 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,12 @@ class User < ApplicationRecord
   has_many :items
   has_many :purchase_records
 
-  validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
+  validates :nickname, presence: true 
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶ一-]+\z/, message: 'is invalid. Input full-width katakana characters' }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶ一-]+\z/, message: 'is invalid. Input full-width katakana characters' }
   validates :birth_day, presence: true
 end
