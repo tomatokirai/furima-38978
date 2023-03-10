a6d50555c3c3233cfb3e42029f96c2b3f930afa8
tomatokirai <124859494+tomatokirai@users.noreply.github.com>
2023-03-10 10:58:59 +0900

    active storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,12 @@
 class ItemsController < ApplicationController
+
+end
+
+def new
+  @items = Irem.new
+
+private
+
+def message_params
+  params.require(:item).permit(:image).merge(user_id: current_user.id)
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+
+  belongs_to :user
+
+  has_one_attached :image
+
+
+  validates :image, presence: true
 end
