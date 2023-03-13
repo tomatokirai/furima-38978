c0fc6bc19eb768c7b3de3821fa673968355bc059
tomatokirai <124859494+tomatokirai@users.noreply.github.com>
2023-03-13 16:00:57 +0900

    商品一覧表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,17 @@
 class ItemsController < ApplicationController
 
   before_action :authenticate_user!, except: [:index]
+  before_action :set_item, only: [:edit, :show]
 
 
   def new
     @item = Item.new
   end
 
+  def index
+    @items = Item.all
+  end
+  
   def create
     @item = Item.new(item_params)
     if @item.save
@@ -16,10 +21,21 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+
 
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :price, :item_text, :category_id, :situation_id, :shipping_id, :delivery_area_id, :delivery_day_id).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
