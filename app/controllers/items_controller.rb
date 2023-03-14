7527f3e52c5325d482967c630cc308fc9af80b74
tomatokirai <124859494+tomatokirai@users.noreply.github.com>
2023-03-14 16:00:05 +0900

    Revert "Revert "編集機能の実装""
    This reverts commit aecb7a9b97a4dd87727f3ca20215989df90d8320.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
 
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show]
+  before_action :set_item, only: [:show, :edit, :update]
 
 
   def new
@@ -24,8 +24,17 @@ class ItemsController < ApplicationController
   def show
   end
 
-  # def edit
-  # end
+  def edit
+  end
+
+  def update
+    
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
 
 
 
