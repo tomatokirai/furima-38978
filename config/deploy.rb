f9b1b0fd81a42ca57610a366e3a499efa71334c0
tomatokirai <124859494+tomatokirai@users.noreply.github.com>
2023-03-21 18:59:19 +0900

    masterをmainに変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,10 @@ set :application, 'furima-38978'
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:tomatokirai/furima-38978.git'
 
+# ブランチを指定する
+set :branch, "main"
+
+
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
